5cb4c76df5ca61284d1cd7d1d5a0df58b4fc31f7
Bradley Stillwagon <bastillwagon@gmail.com>
2024-10-09 18:26:30 -0500

    Add types, update gql schema and resolvers

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,3 +1,98 @@
-import { gql } from "apollo-server-express"
+import { gql } from 'apollo-server-express';
 
-export const typeDefs = gql ``;
+export const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    registrations: [Registration!]!
+  }
+
+  type Event {
+    id: ID!
+    title: String!
+    description: String!
+    date: String!
+    capacity: Int!
+    category: String!
+    registrations: [Registration!]!
+    availableSpots: Int!
+  }
+
+  type Registration {
+    id: ID!
+    user: User!
+    event: Event!
+    registrationDate: String!
+    status: RegistrationStatus!
+  }
+
+  enum RegistrationStatus {
+    CONFIRMED
+    WAITLIST
+    CANCELLED
+  }
+
+  type EventConnection {
+    edges: [EventEdge!]!
+    pageInfo: PageInfo!
+  }
+
+  type EventEdge {
+    cursor: String!
+    node: Event!
+  }
+
+  type PageInfo {
+    hasNextPage: Boolean!
+    endCursor: String
+  }
+
+  input PaginationInput {
+    first: Int!
+    after: String
+  }
+
+  input EventFilterInput {
+    category: String
+    dateFrom: String
+    dateTo: String
+    searchTerm: String
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+    events(
+      pagination: PaginationInput!
+      filter: EventFilterInput
+    ): EventConnection!
+    event(id: ID!): Event
+    registrations: [Registration!]!
+  }
+
+  input CreateUserInput {
+    name: String!
+    email: String!
+  }
+
+  input CreateEventInput {
+    title: String!
+    description: String!
+    date: String!
+    capacity: Int!
+    category: String!
+  }
+
+  input CreateRegistrationInput {
+    userId: ID!
+    eventId: ID!
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User!
+    createEvent(input: CreateEventInput!): Event!
+    createRegistration(input: CreateRegistrationInput!): Registration!
+    cancelRegistration(id: ID!): Registration!
+  }
+`;
